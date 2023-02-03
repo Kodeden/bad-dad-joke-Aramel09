911aa4931724895996ec2fdd49443987623c53aa
Alejandro Melara <alejandro.melara96@gmail.com>
2023-02-03 15:07:29 -0500

    Fetching jokes on the console

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const fetchJokes = () => {
+    fetch("https://official-joke-api.appspot.com/random_joke")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
+  fetchJokes();
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button onClick={() => fetchJokes()}>Joke</button>
+      <div></div>
+      <button>Good Joke</button>
+      <div></div>
+      <button>Bad Joke</button>
+    </>
+  );
 }
 
 export default App;
