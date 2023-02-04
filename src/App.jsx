32575d12772a9b867abfa41be6a2735c25449717
Alejandro Melara <alejandro.melara96@gmail.com>
2023-02-04 07:13:05 -0500

    Displaying jokes in the UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
+import { useState } from "react";
+
 function App() {
+  const [jokes, setJokes] = useState("");
+
   const fetchJokes = () => {
     fetch("https://official-joke-api.appspot.com/random_joke")
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => setJokes(data));
   };
-  fetchJokes();
+
+  const arrayGoodJokes = [];
+  arrayGoodJokes.push(jokes);
+
+  const arrayBadJokes = [];
+  arrayBadJokes.push(jokes);
 
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <button onClick={() => fetchJokes()}>Joke</button>
+      <p>{jokes.setup}</p>
+      <p>{jokes.punchline}</p>
+      {/* <ul>
+        {arrayJokes.map((data) => (
+          <li key={data.id}>
+            {data.setup} - {data.punchline}
+          </li>
+        ))}
+      </ul> */}
       <div></div>
       <button>Good Joke</button>
       <div></div>
