4f2ff3aa0202250592f9305eff66e1bfcae9056a
Alejandro Melara <alejandro.melara96@gmail.com>
2023-02-07 22:23:14 -0500

    Add JokeVote and if condition

--- src/components/JokeText.jsx
+++ src/components/JokeText.jsx
@@ -1,12 +1,21 @@
 import PropTypes from "prop-types";
+import JokeVote from "./JokeVote";
 
-export default function JokeText({ jokes }) {
-  return (
-    <>
-      <p>{jokes?.setup}</p>
-      <p>{jokes?.punchline}</p>
-    </>
-  );
+export default function JokeText({ jokes, handleClick, goodJokes, badJokes }) {
+  if (jokes) {
+    return (
+      <>
+        <p className=" mt-3 font-semibold">{jokes?.setup}</p>
+        <p>{jokes?.punchline}</p>
+
+        <JokeVote
+          handleClick={handleClick}
+          goodJokes={goodJokes}
+          badJokes={badJokes}
+        />
+      </>
+    );
+  }
 }
 
 JokeText.propTypes = {
@@ -14,4 +23,7 @@ JokeText.propTypes = {
     setup: PropTypes.string.isRequired,
     punchline: PropTypes.string.isRequired,
   }),
+  handleClick: PropTypes.func.isRequired,
+  goodJokes: PropTypes.array.isRequired,
+  badJokes: PropTypes.array.isRequired,
 };
