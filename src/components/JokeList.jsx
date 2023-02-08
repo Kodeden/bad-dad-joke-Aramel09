79d337168dc05f363d0429ae2c8aae47df69202c
Alejandro Melara <alejandro.melara96@gmail.com>
2023-02-07 22:21:48 -0500

    Added prop tittle and CSS to JokeList

--- src/components/JokeList.jsx
+++ src/components/JokeList.jsx
@@ -1,19 +1,24 @@
 import PropTypes from "prop-types";
 
-export default function JokeList({ Jokes }) {
+export default function JokeList({ jokes, tittle }) {
   return (
     <>
-      <ul>
-        {Jokes.map((data) => (
-          <li key={data.id}>
-            {data.setup} - {data.punchline}
-          </li>
-        ))}
-      </ul>
+      <div className="flex flex-col p-2">
+        <h2 className=" font-semibold">{tittle}</h2>
+
+        <ul>
+          {jokes.map((data) => (
+            <li key={data.id}>
+              {data.setup} - {data.punchline}
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
 
 JokeList.propTypes = {
-  Jokes: PropTypes.array.isRequired,
+  jokes: PropTypes.array.isRequired,
+  tittle: PropTypes.string.isRequired,
 };
