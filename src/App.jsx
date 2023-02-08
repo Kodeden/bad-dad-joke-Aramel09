ade2c8176af3d69678691bf1b468bae51ea60f89
Alejandro Melara <alejandro.melara96@gmail.com>
2023-02-07 22:25:30 -0500

    Add CSS
    Add JokeList and move JokeVote to JokeText

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
 import JokeButton from "./components/JokeButton";
+import JokeList from "./components/JokeList";
 import JokeText from "./components/JokeText";
-import JokeVote from "./components/JokeVote";
 import useJoke from "./hooks/useJoke";
 
 function App() {
   const [jokes, setJokes, goodJokes, handleClick, badJokes] = useJoke();
 
   return (
-    <>
-      <JokeButton setJokes={setJokes} />
+    <main className=" flex h-screen flex-col items-center justify-center">
+      <div className="mb-20 flex flex-col items-center">
+        <JokeButton setJokes={setJokes} />
 
-      <JokeText jokes={jokes} />
-      <JokeVote
-        handleClick={handleClick}
-        goodJokes={goodJokes}
-        badJokes={badJokes}
-      />
-    </>
+        <JokeText
+          jokes={jokes}
+          handleClick={handleClick}
+          goodJokes={goodJokes}
+          badJokes={badJokes}
+        />
+      </div>
+      <section className="flex gap-10 ">
+        <JokeList jokes={goodJokes} tittle="Good Joke" />
+        <JokeList jokes={badJokes} tittle="Bad Joke" />
+      </section>
+    </main>
   );
 }
 
